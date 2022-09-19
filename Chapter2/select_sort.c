58e9372bab2b7cfc74e6bdbe29819b25b3c3a4e1
jaewoopk <jaewoopk000@naver.com>
2022-09-19 17:06:01 +0900

    Day 3 Solve for 2-1

---
+++ Chapter2/select_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+void insertion(int* arr, int idx) {
+    int max = arr[0], id = 0, tmp;
+    for (int i = 0; i <= idx; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            id = i;
+        }
+    }
+    tmp = arr[idx];
+    arr[idx] = arr[id];
+    arr[id] = tmp;
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    int *arr = (int *)malloc(sizeof(int) * n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d",&arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        insertion(arr, n - i - 1);
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ",arr[i]);
+    }
+    free(arr);
+    return (0);
+}
+
+//8
+//8 31 48 73 3 65 20 29
