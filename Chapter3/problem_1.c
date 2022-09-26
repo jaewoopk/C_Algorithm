54c7d031f8c82da659d89f918d2f92a51875ef29
jaewoopk <jaewoopk000@naver.com>
2022-09-26 18:01:26 +0900

    Day 5 Trying to solve but failed

---
+++ Chapter3/problem_1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+int H[100];
+int n = 1;
+
+void insertItem(int key);
+int removeMax();
+void upHeap(int i);
+void downHeap(int i);
+void printHeap();
+
+int main(void) {
+    int k;
+    while (1) {
+        if (n >= 100)
+            exit(0);
+        char ch;
+        scanf("%c",&ch);
+        getchar();
+        switch (ch)
+        {
+        case 'i' :
+            scanf("%d",&k);
+            getchar();
+            insertItem(k);
+            break;
+        
+        case 'd' :
+            printf("%d\n",removeMax());
+            break;
+
+        case 'p' :
+            printHeap();
+            break ;
+        case 'q' :
+            exit(0);
+        }
+    }
+    return (0);
+}
+
+void insertItem(int key) {
+    H[n] = key;
+    upHeap(n);
+    n++;
+    printf("0\n");
+}
+
+int removeMax() {
+    int tmp = H[1];
+    H[1] = H[n - 1];
+    downHeap(1);
+    n--;
+    return (tmp);
+}
+
+void upHeap(int i) {
+    if (i == 1) return ;
+    else if (H[i] > H[i / 2]) {
+        int tmp = H[i];
+        H[i] = H[i / 2];
+        H[i / 2] = tmp;
+        upHeap(i / 2);
+    }
+}
+
+void downHeap(int i) {
+    if (i * 2 >= n) return ;
+    printHeap();
+    if (H[i] < H[(i * 2) + 1]) {
+        int tmp = H[i];
+        H[i] = H[(i * 2) + 1];
+        H[(i * 2) + 1] = tmp;
+        downHeap((i * 2) + 1);
+    }
+    if (H[i] < H[i * 2]) {
+        int tmp = H[i];
+        H[i] = H[i * 2];
+        H[i * 2] = tmp;
+        downHeap(i * 2);
+    }
+}
+
+void printHeap() {
+    for (int i = 1; i < n; i++) {
+        printf(" %d",H[i]);
+    }
+    printf("\n");
+}
