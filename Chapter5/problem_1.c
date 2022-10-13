302192eb66f42e3cf7f5b5b24cdd4a546d3cc5ab
jaewoopk <jaewoopk000@naver.com>
2022-10-13 18:34:40 +0900

    Day 12 To get merge sort works it

--- Chapter5/problem_1.c
+++ Chapter5/problem_1.c
@@ -12,6 +12,7 @@ typedef struct s_list {
 } t_list;
 
 void insertItems(t_list *list, int data);
+void printList(t_list *list);
 t_list *mergeSort(t_list *list);
 t_list *merge(t_list *list1, t_list *list2, int k);
 t_list *partition(t_list *list,int k);
@@ -32,11 +33,7 @@ int main(void) {
     }
 
     t_list *answer = mergeSort(list);
-    tmp = answer->head;
-    while (tmp->next) {
-        printf(" %d",tmp->next->data);
-        tmp = tmp->next;
-    }
+    printList(answer);
     exit(0);
     return (0);
 }
@@ -54,25 +51,12 @@ void insertItems(t_list *list, int data) {
 }
 
 t_list *mergeSort(t_list *list) {
-    t_list *tmp1 = partition(list, 4);
-    t_list *tmp2 = partition(list, 2);
-    t_list *tmp3 = partition(list, 6);
-    t_list *tmp4 = partition(list, 1);
-    t_list *tmp5 = partition(list, 3);
-    t_list *tmp6 = partition(list, 5);
-    t_list *tmp7 = partition(list, 7);
-    t_list *tmp8 = partition(list, 8);
-    
-    t_list *ans1 = merge(tmp4, tmp2, 2);
-    t_list *ans2 = merge(tmp5, tmp1, 2);
-    t_list *ans3 = merge(tmp6, tmp3, 2);
-    t_list *ans4 = merge(tmp7, tmp8, 2);
-
-    t_list *answer1 = merge(ans1, ans2, 4);
-    t_list *answer2 = merge(ans3, ans4, 4);
-
-    t_list *real_answer = merge(answer1, answer2, 8);
-    return real_answer;
+    t_list *list2;
+    list2 = partition(list, 4);
+    printList(list);
+    printf("\n----------\n");
+    printList(list2);
+    return list2;
 }
 
 t_list *merge(t_list *list1, t_list *list2, int k) {
@@ -99,12 +83,21 @@ t_list *merge(t_list *list1, t_list *list2, int k) {
 
 t_list *partition(t_list *list, int k) {
     t_list *newList = (t_list *)malloc(sizeof(t_list));
-    t_node *tmp = list->head;
+    t_node *tmp = list->head->next;
     newList->head = (t_node *)malloc(sizeof(t_node));
 
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k - 1; i++) {
         tmp = tmp->next;
     }
-    newList->head->next = tmp;
+    newList->head->next = tmp->next;
+    tmp->next = NULL;
     return newList;
+}
+
+void printList(t_list *list) {
+    t_node *tmp = list->head->next;
+    while (tmp) {
+        printf(" %d ,",tmp->data);
+        tmp = tmp->next;
+    }
 }
