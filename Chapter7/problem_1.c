a1124f7207a2212f5cfcbe335b90ffb8cb9b316c
jaewoopk <jaewoopk000@naver.com>
2022-10-30 21:18:22 +0900

    Day 22 Trying to solve it about binary search tree

--- Chapter7/problem_1.c
+++ Chapter7/problem_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #pragma warning(disable:4996)
 
 typedef struct s_node {
@@ -9,16 +10,70 @@ typedef struct s_node {
     int key;
 } t_node;
 
-int findElement(int key);
-void insertItem(int key);
-t_node *treeSearch(int key);
-int removeElement(int key);
+int findElement(int k);
+void insertItem(t_node *node, int k);
+t_node *treeSearch(int k);
+void printTree(t_node *node);
+int removeElement(int k);
 bool isExternal(t_node *w);
 bool isInternal(t_node *w);
 t_node *inOrderSucc(t_node *w);
 
 int main(void) {
-	t_node *root = (t_node *)malloc(sizeof(t_node));
+	t_node *root;
+    char c;
+    int k;
+    bool quit = true;
 
+    while (quit) {
+        scanf(" %c %d", &c, &k);
+        switch (c)
+        {
+        case 'i' :
+            insertItem(root, k);
+            break ;
+        
+        case 'p' :
+            printTree(root);
+            break ;
+        case 'q' :
+            quit = false;
+            break ;
+        default:
+            break;
+        }
+    }
     return (0);
+}
+
+// int findElement(int k) {
+
+// }
+
+void insertItem(t_node *node, int k) {
+    if (!node) {
+        t_node *newNode = (t_node *)malloc(sizeof(t_node));
+        newNode->key = k;
+        node = newNode;
+        return ;
+    }
+    if (node->key < k) {
+        insertItem(node->rChild, k);
+        node->rChild->parent = node;
+    }
+    else {
+        insertItem(node->lChild, k);
+        node->lChild->parent = node;
+    }
+}
+
+void printTree(t_node *node) {
+    if (!node) {
+        return ;
+    }
+    else {
+        printf(" %d",node->key);
+        printTree(node->lChild);
+        printTree(node->rChild);
+    }
 }
