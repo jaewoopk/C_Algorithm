6fff24b4594077f18394cb127f70cb63978ab6b2
jaewoopk <jaewoopk000@naver.com>
2022-12-11 21:07:29 +0900

    Day 36 Trying to solve problem with MST

---
+++ Chapter12/problem_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int n, m;
+
+int main() {
+    scanf("%d %d",&n, &m);
+    int G[n + 1][n + 1];
+    for (int i = 1; i < m + 1; i++) {
+        
+    }
+    return 0;
+}
